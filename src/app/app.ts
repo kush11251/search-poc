a26889e1df4a562fe2834f95be4451bdac04727d
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-04-10 10:47:12 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class App {
+  
+}
+
